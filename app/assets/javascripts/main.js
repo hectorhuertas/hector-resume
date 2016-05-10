f3c5bc64d7746d52ac23868dcee54b7220e34748
Hector Huertas <hectorhuertas@gmail.com>
2016-05-10 19:29:21 +0200

    Extract render module

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
   $.get('/api/v1/resume')
-   .then(renderResume);
+   .then(Render.resume);
 });
-
-function renderResume(resume) {
-  var resume_html = '';
-
-  for (var section in resume) {
-    resume_html = resume_html + HtmlFor[section](resume[section]);
-  }
-
-  $('body').append(resume_html);
-}
